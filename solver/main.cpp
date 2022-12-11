23fa10a87c8410ff92d70f4d6e767a07f3ae2b91
Maxim Kogan <mkogan58@gmail.com>
2022-12-11 15:38:43 +0300

    Add console param for solver type

--- solver/main.cpp
+++ solver/main.cpp
@@ -1,12 +1,14 @@
-#include <cassert>
 #include <iostream>
-
-#include "semi_implicit/semi_implicit_solver.h"
+#include <memory>
 #include <mpi.h>
 
-const int kMasterRank = 0;
+#include "semi_implicit/semi_implicit_solver.h"
 
-int main(int argc, char **argv) {
+const int kMasterRank = 0;
+const std::string kSemiImplicitSolverType = "semi-implicit";
+const std::string kExplicitSolverType = "explicit";
+
+int main(int argc, char** argv) {
   MPI_Init(&argc, &argv);
 
   int process_count;
@@ -15,28 +17,52 @@ int main(int argc, char **argv) {
   MPI_Comm_size(MPI_COMM_WORLD, &process_count);
   MPI_Comm_rank(MPI_COMM_WORLD, &current_rank);
 
-  if (current_rank == kMasterRank && argc != 3) {
+  if (current_rank == kMasterRank && argc < 3) {
     std::cerr << "The wrong number of arguments. Required 2, but got "
               << argc - 1 << "." << std::endl;
-    std::cerr << "Usage: [input-file-path] [output-file-path]" << std::endl;
+    std::cerr << "Usage: [input-file-path] [output-file-path] [solver-type, default: "
+              << kSemiImplicitSolverType << "]"
+              << std::endl;
     return 1;
   }
 
   std::string input_filepath = argv[1];
   std::string output_filepath = argv[2];
+  std::string solver_type = (argc == 3 ? kSemiImplicitSolverType : argv[3]);
+
+  if (solver_type != kSemiImplicitSolverType && solver_type != kExplicitSolverType) {
+    std::cerr << "Invalid solver type! Must be one of [" << kSemiImplicitSolverType << ", "
+              << kExplicitSolverType << "]" << std::endl;
+    return 1;
+  }
+
   auto properties = PropertiesManagerFabric::Create(input_filepath);
 
   std::ofstream out(output_filepath, std::ios::out);
   if (out.fail()) {
     std::cerr << "An error occurred while opening the input file" << std::endl;
-    assert(false);
+    return 2;
   }
 
-  SemiImplicitSolver solver(current_rank, process_count, &properties, [&](const Matrix &matrix) {
+  auto callback = [&out](const Matrix& matrix) {
     out << matrix;
     out << "\n\n";
-  });
-  solver.Solve();
+  };
+
+  std::shared_ptr<SolverBase> solver;
+
+  if (solver_type == kSemiImplicitSolverType) {
+    solver = std::make_shared<SemiImplicitSolver>(
+        current_rank,
+        process_count,
+        &properties,
+        callback
+    );
+  } else if (solver_type == kExplicitSolverType) {
+    // TODO: solver = std::make_shared<ExplicitSolver>(...);
+  }
+
+  solver->Solve();
 
   out.close();
 
